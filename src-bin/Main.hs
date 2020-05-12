b7bfb8430a3ce4a1d8ad87fc17aa63750d57138b
Divam <dfordivam@gmail.com>
2020-05-12 13:09:56 +0900

    Init cabal project

---
+++ src-bin/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Language.Javascript.JSaddle.Benchmark
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
---
+++ src/Language/Javascript/JSaddle/Benchmark.hs
@@ -0,0 +1,1 @@
+module Language.Javascript.JSaddle.Benchmark where
