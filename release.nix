8503c1621d732ee66b6ea82453b6456021f96836
Divam <dfordivam@gmail.com>
2020-08-20 16:54:39 +0900

    Add reflex project build products to release.nix

--- release.nix
+++ release.nix
@@ -1,11 +1,19 @@
-{
-  exp-core = (import ./shell.nix {}).exp-core;
-  master = (import ./shell.nix {}).master;
-  exp-core-android = (import ./reflex-project.nix {
+let
+  exp-core-reflex-project = (import ./reflex-project.nix {
+      reflex-platform = import ./reflex-platform;
       appNameSuffix = "_exp_core";
-    }).android.jsaddle-benchmark;
-  master-android = (import ./reflex-project.nix {
+  });
+  master-reflex-project = (import ./reflex-project.nix {
       reflex-platform = import ./reflex-platform-master;
       appNameSuffix = "_master";
-    }).android.jsaddle-benchmark;
+  });
+in {
+  exp-core = (import ./shell.nix {}).exp-core;
+  master = (import ./shell.nix {}).master;
+  exp-core-android = exp-core-reflex-project.android.jsaddle-benchmark;
+  exp-core-ghc = exp-core-reflex-project.ghc.jsaddle-benchmark;
+  exp-core-ghcjs = exp-core-reflex-project.ghcjs.jsaddle-benchmark;
+  master-android = master-reflex-project.android.jsaddle-benchmark;
+  master-ghc = master-reflex-project.ghc.jsaddle-benchmark;
+  master-ghcjs = master-reflex-project.ghcjs.jsaddle-benchmark;
 }
